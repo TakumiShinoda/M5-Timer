83af957e923cfc9184391a19a0cf301763f53b0b
TakimiShinoda <godgodx4@gmail.com>
2018-09-22 11:03:37 +0900

    add stop button

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <M5Stack.h>
 
 uint8_t Time = 60;
+uint16_t TempTime = 0;
 long Start = 0;
-bool ClockState = false; 
+bool ClockState = false;
 
 void updateTimeDisplay(){
   m5.Lcd.setTextSize(7);
@@ -21,6 +22,7 @@ void loop(){
   if(M5.BtnA.wasPressed()){
     Time = 60;
     ClockState = false;
+    Start = 0;
   }
 
   if(M5.BtnB.wasPressed()){  
@@ -31,13 +33,21 @@ void loop(){
   }
 
   if(M5.BtnC.wasPressed()){
-    ClockState = !ClockState;
+    ClockState = !ClockState; 
+    if(ClockState){
+      if(Start != 0 && TempTime != 0){
+        Start = now;
+      }
+    }else{
+      TempTime = now - Start;
+    }
   }
 
   if(ClockState){
-    if(now - Start > 1000){
+    if((now - Start) + TempTime > 1000){
       Time -= 1;
       Start = now;
+      TempTime = 0;
     }
   }
 
